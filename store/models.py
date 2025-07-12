0b076863c2802830540a6e879d5a861cc561c8c3
fazal <fazalktk93@gmail.com>
2025-07-12 22:02:04 +0500

    fdsfafdsa

--- store/models.py
+++ store/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-class Office(models.Model):
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200, blank=True)
-
-    def __str__(self):
-        return self.name
+class OfficeCreateView(CreateView):
+    model = Office
+    fields = ['name', 'location']  # ✅ This will auto-generate the form
+    template_name = 'store/office_form.html'
+    success_url = reverse_lazy('office_list')
 
 class Vendor(models.Model):
     name = models.CharField(max_length=200)
