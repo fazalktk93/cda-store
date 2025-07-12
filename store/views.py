3fb31aac789f7cf3f5dd183f14510145f72e6c9d
fazal <fazalktk93@gmail.com>
2025-07-12 23:31:07 +0500

    fdsfa

--- store/views.py
+++ store/views.py
@@ -63,13 +63,13 @@ def issue_create(request):
         form.save()
         return redirect('stock_list')
 
-    # Stock quantities for JS
+    # 👇 Prepare data for the dropdown quantity display
     stock_data = {str(item.id): item.quantity for item in StockItem.objects.all()}
     stock_data_json = json.dumps(stock_data, cls=DjangoJSONEncoder)
 
     return render(request, 'store/issue_form.html', {
         'form': form,
-        'stock_data_json': stock_data_json
+        'stock_data_json': stock_data_json,  # 👈 pass to template
     })
 
 # PDF Report
