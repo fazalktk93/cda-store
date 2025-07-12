9ceaa2c451f0dee1b1d14396a997ca8f9027aaf2
fazal <fazalktk93@gmail.com>
2025-07-12 23:08:39 +0500

    fdsf

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include  # ✅ Correct import
 from django.urls import path
 from . import views
-from .views import OfficeListView, OfficeCreateView, report_view
+from .views import OfficeListView, OfficeCreateView, report_form_view, report_view
 from .views import report_search    
 from .views import report_view
 
@@ -16,7 +16,7 @@ urlpatterns = [
     path('offices/', OfficeListView.as_view(), name='office_list'),
     path('offices/add/', OfficeCreateView.as_view(), name='office_add'),
     path('report/', report_view, name='report_view'),
-    path('report/form/', report_view, name='report_form'),
+    path('report/form/', report_form_view, name='report_form'),
     path('report/pdf/', views.report_pdf, name='report_pdf'),
     path('report/search/', report_search, name='report_search'),
     path('accounts/', include('django.contrib.auth.urls')),  # login/logout
